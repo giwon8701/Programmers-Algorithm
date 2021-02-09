71359100123022ca7783273d7486e490db61da25
Projects - Qclass_KH <l4279625@gmail.com>
2021-02-10 00:32:51 +0900

    프로그래머스 완벽하게 풀었다!

--- Java/src/programmers01_72410.java
+++ Java/src/programmers01_72410.java
@@ -6,17 +6,20 @@ https://programmers.co.kr/learn/courses/30/lessons/72410?language=java
 public class programmers01_72410 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String ex1 = "...!@BaT#*..y.abcdefghijklm";
+        String ex1 = "...!@BaT#*..y.abcdefghijklm*";
         String ex2 = "z-+.^.";
         String ex3 = "=.=";
         String ex4 = "123_.def";
         String ex5 = "abcdefghijklmn.p";
-        solution(ex1);
+        String ex6 = "~!@#$%^&()=+[{]}:?,<>";
+        String ex7 = "3.2............3";
+        String ex8 = ".1.";
+        String ex9 = ".......b";
+        String ex10 = "...abc...";
+        solution(ex3);
     }
     // 프로그래머스에서는 void가 아닌 String을 반환해줌
-    public static void solution(String new_id) {
+    public static String solution(String new_id) {
         StringBuilder answer = new StringBuilder();
         char[] id_arr = new_id.toLowerCase().toCharArray();
         List<Character> id = new LinkedList<>();
@@ -28,6 +31,9 @@ public class programmers01_72410 {
         step_five(id);
         step_six(id);
         step_seven(id, answer);
+
+
+        return answer.toString();
 
     }
 
@@ -48,12 +54,15 @@ public class programmers01_72410 {
     // 2단계 (소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거)
     private static void step_two(List<Character> id) {
         /*--------------------Start----------------------*/
-        for (int i = 0; i < id.size(); i++) {
+        int i = 0;
+        while (i != id.size()) {
+//        for (int i = 0; i < id.size(); i++) {
             if (((id.get(i) < '0' || id.get(i) > '9') &&                   // 숫자가 아닐 때
                     (id.get(i) < 'a' || id.get(i) > 'z')) &&                     // 알파벳이 아닐 때
                     (id.get(i) != '-' && id.get(i) != '_' && id.get(i) != '.')) {  // 빼기, 밑줄, 마침표 가 아닐 때
                 id.remove(i);
-                i = 0;
+            } else {
+                i++;
             }
         }
         /*-----------------------------------------------*/
@@ -63,18 +72,17 @@ public class programmers01_72410 {
         }
         System.out.println();
     }
-    // 3단계 (해당문자와 다음문자가 마침표일 때 현재문자 삭제)
+    // 3단계 (마침표가 연달아 나올 때 현재문자 삭제)
     private static void step_three(List<Character> id) {
         /*--------------------Start----------------------*/
         int cnt = 0;
-        while (!id.isEmpty()) {
+        while (cnt != id.size()) {
             if ((cnt != id.size() - 1) && id.get(cnt).equals('.') && id.get(cnt).equals(id.get(cnt + 1))) {   // Integer는 객체이므로 .equals()로 비교해야한다.
                 id.remove(cnt);
                 cnt = 0;
             } else {
                 cnt++;
             }
-            if (cnt == id.size()) break;
         }
         /*-----------------------------------------------*/
         System.out.print("3단계: ");
@@ -87,14 +95,14 @@ public class programmers01_72410 {
     private static void step_four(List<Character> id) {
         /*--------------------Start----------------------*/
         // 첫 인덱스가 마침표일 때 (size가 1이면 여기서 지워줌으로써 size=0이 됨)
-        if (id.get(0).equals('.')) {
-            id.remove(0);
-        }
-        // 마지막 인덱스가 마침표일 때
-        // (바로 위에서 지워버려서 size가 0이면 지울것이 없기 때문에 size가 0인경우는 제외)
-        if (id.size() != 0 && id.get(id.size() - 1).equals('.')) {
-            id.remove(id.size() - 1);
-        }
+            if (id.size() > 0 && id.get(0).equals('.')) {
+                id.remove(0);
+            }
+            // 마지막 인덱스가 마침표일 때
+            // (바로 위에서 지워버려서 size가 0이면 지울것이 없기 때문에 size가 0인경우는 제외)
+            if (id.size() > 0 && id.get(id.size() - 1).equals('.')) {
+                id.remove(id.size() - 1);
+            }
         /*-----------------------------------------------*/
         System.out.print("4단계: ");
         for (Character ch : id) {
