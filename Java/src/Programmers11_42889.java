61ee037f2eb45c4a30fc67ff0ade95e337621832
Giwon Lim <72638829+giwon8701@users.noreply.github.com>
2021-04-16 21:23:02 +0900

    실패율 푸는중

--- Java/src/Programmers11_42889.java
+++ Java/src/Programmers11_42889.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /*
 https://programmers.co.kr/learn/courses/30/lessons/42889
@@ -8,50 +6,45 @@ https://programmers.co.kr/learn/courses/30/lessons/42889
 public class Programmers11_42889 {
 
     public static void main(String[] args) {
-
+        int[] N = {5, 4};
+        int[][] stages = {
+                {2, 1, 2, 6, 2, 4, 3, 3},
+                {4, 4, 4, 4, 4}
+        };
+        int i = 0;
+        solution(N[i], stages[i]);
     }
 
     public static int[] solution(int N, int[] stages) {
-        int[] answer= new int[N];
-        Map<Integer, Integer> fail = new HashMap<>();
-        fail.put(0, 0);
-        fail.put(N+2, 0);
-        Map<Integer, Integer> clearUserNum = new HashMap<>();
-        Map<Integer, Integer> tryingUserNum = new HashMap<>();
-        for (int i=0; i<N; i++) {
-            clearUserNum.put(i, 0);
-        }
+        int answer[] = new int[N];
+        Map<Float, Float> stages_failure = new TreeMap<>(); // 스테이지별 실패율
+        int[] fail = new int[N];
+        int[] success = new int[N];
 
         for (int stage=1; stage<=N; stage++) {
-            int clearCnt = 0;
-            int tryingCnt = 0;
-            for (int trying : stages) {
-                if (trying > stage) clearCnt++;
-                if (trying <= stage) tryingCnt++;
+            for (int user : stages) {
+                // 스테이지를 도전중인 사람 수
+                if (stage == user) fail[stage - 1]++;
+
+                // 해당 유저가 현재 탐색중인 스테이지를 통과한 상태일 때 성공한 사람을 카운트해줌
+                if (stage < user) success[stage - 1]++;
             }
-            clearUserNum.replace(stage, clearUserNum.get(stage)+clearCnt);
-            tryingUserNum.replace(stage, tryingUserNum.get(stage)+tryingCnt);
+
+            float failure = (float) fail[stage - 1] / (float) (success[stage - 1] + fail[stage - 1]);
+            stages_failure.put(stage, failure);
         }
 
         for (int i=1; i<=N; i++) {
-            if (clearUserNum.get(i) == 0) {
-                fail.put(i, 0);
-            } else {
-                fail.put(i, tryingUserNum.get(i) / clearUserNum.get(i));
-            }
+            System.out.println(stages_failure.get(i));
         }
 
-
-        for(int i=1; i<=fail.size(); i++) {
-            for (int j=1; j<fail.size(); j++) {
-                if (fail.get(i) > fail.get(j)) {
-
-                }
-            }
-        }
+        List<Integer> arrList = new ArrayList<>(stages_failure.keySet());
+        Collections.sort(arrList, (k, v) -> stages_failure.get(k.compareTo(stages_failure.get(v))));
 
 
 
-        return answer;
+
+
+        return null;
     }
 }
