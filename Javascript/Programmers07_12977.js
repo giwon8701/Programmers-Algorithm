5d0f90cdcd2d6a7c6f086c9d02839763722281ab
Projects - Qclass_KH <l4279625@gmail.com>
2021-04-24 00:11:40 +0900

    js로 소수만들기 음양더하기 해봄

---
+++ Javascript/Programmers07_12977.js
@@ -0,0 +1,19 @@
+
+
+let nums = [
+    [1, 2, 3, 4],
+    [1, 2, 7, 6, 4]
+];
+
+
+function solution(nums) {
+    var answer = -1;
+
+    for (let j=0; j<nums.length; j++) {
+           nums[j] = 0;
+    }
+    
+}
+
+let i = 0;
+solution(nums[i]);
---
+++ Javascript/Programmers07_76501.js
@@ -0,0 +1,18 @@
+
+let absolutes = [
+    [4, 7, 12],
+    [1, 2, 3]
+];
+let signs = [
+    [true, false, true],
+    [false, false, true]
+];
+let i = 0;
+
+
+let res = absolutes[i];
+console.log(typeof(res))
+
+function solution(absolutes[i]) {
+    
+}
