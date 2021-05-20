7162f271fe6adceba104f3e7e879ac3887ea7f5d
Projects - Qclass_KH <l4279625@gmail.com>
2021-05-20 20:50:05 +0900

    level1 비밀지도

---
+++ level1_비밀지도.js
@@ -0,0 +1,12 @@
+function solution(n, arr1, arr2) {
+    let answer = [];
+
+    for (let i=0; i<n; i++) {
+        answer[i] = (arr1[i] | arr2[i]).toString(2).padStart(n, '0').replace(/1/g, "#").replace(/0/g, " ");
+    }
+
+    return answer;
+}
+
+
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
