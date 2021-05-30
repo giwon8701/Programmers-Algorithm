ae2f4964ae7cdd5579bfc2ec3fc7c7765bf0eb73
Projects - Qclass_KH <l4279625@gmail.com>
2021-05-31 03:37:47 +0900

    로또의 최고 순위와 최저 순위

---
+++ level1_로또의_최고_순위와_최저_순위.js
@@ -0,0 +1,11 @@
+function solution(lottos, win_nums) {
+    var answer = [7, 7];
+    const cnt = lottos.filter(num => (num === 0)).length;   // 0의 개수
+    
+    win_nums.map(num => (lottos.includes(num)) ? (answer[0] -= 1, answer[1] -= 1) : '');
+    
+    answer[0] -= cnt;
+    answer[1] = (answer[1] === 7) ? 6 : answer[1];
+    
+    return answer;
+}
