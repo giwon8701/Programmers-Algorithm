1de47856a7a0521c8a2b3927e43f884fb413d4c4
Projects - Qclass_KH <l4279625@gmail.com>
2021-06-07 23:18:19 +0900

    카펫

--- level2_카펫.py
+++ level2_카펫.py
@@ -6,7 +6,7 @@ def solution(brown, yellow):    # 가로x세로 = 넓이(=면적)(=brown+yellow)
     answer = []
 
     # 가로 세로는
-    for i in range(int(yellow**(1/2)), 0, -1):    # 어차피 yellow는
+    for i in range(int(yellow**(1/2)), 0, -1):
         width = yellow//i + 2   # yellow의 가로 길이
         height = i + 2          # yellow의 세로 길이
         if width >= height and brown == height*2 + (width-2) * 2:
